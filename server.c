ea1f5b187165fa9756b2b2f2c7b5651f392b1e38
tutenstein <58851436+tutenstein@users.noreply.github.com>
2021-12-22 10:39:36 +0300

    Update server.c

--- server.c
+++ server.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/types.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
@@ -17,9 +16,8 @@ void error(const char *msg ){
 int main(int argc, char *argv[])
 {
 
-    if (argc < 3){
-        fprintf(stderr, "Do not protocol type or port no provided.Program terminated\n");
-        exit(1);}
+    if (argc < 3)
+        error("Do not protocol type or port no provided.Program terminated\n");
     int portno = atoi(argv[2]);
     int protocoltype= atoi(argv[1]);
     if (protocoltype == 1){
@@ -39,7 +37,7 @@ int main(int argc, char *argv[])
         serv_addr.sin_port = htons(portno);
 
         if (bind(sockfd, (struct sockaddr *)&serv_addr , sizeof(serv_addr))<0)
-             error("binding failed");
+            error("binding failed");
 
         printf("bind done\n");
         listen(sockfd,5);
@@ -66,63 +64,64 @@ int main(int argc, char *argv[])
                 error("Error on writting");
             int i = strncmp("exit", buffer, 4);
             if (i==0)
-               break;
-   }
+                break;
+        }
 
-   close(newsockfd);
-   close(sockfd);
+        close(newsockfd);
+        close(sockfd);
     }
     else if (protocoltype == 2){
 
         int sockfd,n;
         char server_message[255], client_message[255];
         struct sockaddr_in serv_addr,cli_addr;
-        int client_struct_length = sizeof(cli_addr);
+        int clilen = sizeof(cli_addr);
 
 
-       sockfd = socket(AF_INET,SOCK_DGRAM,IPPROTO_UDP);
-       if (sockfd<0)
-           perror("Socket is not created.");
-       printf("Socket is Created.\n");
+        sockfd = socket(AF_INET,SOCK_DGRAM,IPPROTO_UDP);
+        if (sockfd<0)
+            error("Socket is not created.");
+        printf("Socket is Created.\n");
 
-       bzero((char *)&serv_addr, sizeof(serv_addr));
-       serv_addr.sin_family = AF_INET;
-       serv_addr.sin_addr.s_addr = INADDR_ANY;
-       serv_addr.sin_port = htons(portno);
+        bzero((char *)&serv_addr, sizeof(serv_addr));
+        serv_addr.sin_family = AF_INET;
+        serv_addr.sin_addr.s_addr = INADDR_ANY;
+        serv_addr.sin_port = htons(portno);
 
-       if (bind(sockfd,(struct sockaddr*)&serv_addr,sizeof(serv_addr))<0)
-            perror("Binding is failed");
-       printf("binding is done.\n");s
+        if (bind(sockfd,(struct sockaddr*)&serv_addr,sizeof(serv_addr))<0)
+            error("Binding is failed");
+        printf("binding is done.\n");
 
-       while(1){
-   	       printf("Waiting for data...\n");
-	       fflush(stdout);
+        while(1){
+            printf("Waiting for data...\n");
+            fflush(stdout);
 
 
-           bzero(client_message, 255);
-           bzero(server_message, 255);
+            bzero(client_message, 255);
+            bzero(server_message, 255);
 
-           n = recvfrom(sockfd, client_message, sizeof(client_message), 0,(struct sockaddr*)&cli_addr, &client_struct_length);
-           if (n<0)
-               error("Error on reading");
-           printf("Received packet from %s:%d\n", inet_ntoa(cli_addr.sin_addr), ntohs(cli_addr.sin_port));
-           printf("Client: %s\n", client_message);
-           printf("Enter the mesage: ");
-           fgets(server_message,255,stdin);
-           n = sendto(sockfd, server_message, strlen(server_message), 0,(struct sockaddr*)&cli_addr, client_struct_length);
-           if (n<0)
-               error("Error on writting");
-           int i = strncmp("exit", server_message, 4);
-           if (i==0)
-              break;
-              }
-      close(sockfd);
+            n = recvfrom(sockfd, client_message, sizeof(client_message), 0,(struct sockaddr*)&cli_addr, &clilen);
+            if (n<0)
+                error("Error on reading");
+            printf("Received packet from %s:%d\n", inet_ntoa(cli_addr.sin_addr), ntohs(cli_addr.sin_port));
+            printf("Client: %s\n", client_message);
+            printf("Enter the mesage: ");
+            fgets(server_message,255,stdin);
+            n = sendto(sockfd, server_message, sizeof(server_message), 0,(struct sockaddr*)&cli_addr, clilen);
+            if (n<0)
+                error("Error on writting");
+            int i = strncmp("exit", server_message, 4);
+            if (i==0)
+                break;
+        }
+        close(sockfd);
 
     }
 
     else{
         error("protocol type is incorrect. Program terminated.");
-        }
+    }
     return 0;
 }
+
 
